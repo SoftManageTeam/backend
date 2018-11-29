e3931ba9623d45b4b2333937ad3bdac5036d68a7
syj <1803727297@qq.com>
2018-11-29 19:27:11 +0800

    1.0v

--- src/main/java/com/bieezhe/controller/indexController.java
+++ src/main/java/com/bieezhe/controller/indexController.java
@@ -29,7 +29,7 @@ public class indexController {
 	private customerRepository customerRepository;
 	
 	@Autowired
-	private foodRepository foodRepository;
+	private foodRepository foodRepository; 
 	
 	@Autowired 
 	private IndexService indexService;
